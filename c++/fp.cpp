a2cf00cb0f4382b6eff9beb79c358f7d1f4568e0
sneha <ponchupd@gmail.com>
2026-05-01 11:14:40 +0530

    setup

---
+++ c++/fp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+using namespace std;
+int main(){
+int size=5;
+int array[]={2,3,4,7,6};
+for(int i=size-1;i>=0;i--){
+       cout<<array[i]<<" ";
+}
+  cout<<endl;
+  return 0;
+}
