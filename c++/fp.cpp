366781dc6388d6e4295b61fce1be5763351a3a88
sneha <ponchupd@gmail.com>
2026-05-01 22:22:20 +0530

    squaring number

--- c++/fp.cpp
+++ c++/fp.cpp
@@ -4,35 +4,26 @@
 #include <cmath>
 #include <string>
 using namespace std;
-void swap(vector <int> &v){
+void squareandsort(vector <int> &v){
   int size=v.size();
-  int left_ptr = 0;
-  int right_ptr=size-1;
-  while(left_ptr<right_ptr){
-    if(v[left_ptr]%2!=0 && v[right_ptr]%2==0){
-       swap(v[left_ptr],v[right_ptr]);
-       left_ptr++;
-       right_ptr--;
-    }
-    if(v[left_ptr]%2==0){
-      left_ptr++;
-    }
-    if(v[right_ptr]%2!=0){
-      right_ptr--;
-    } 
-  }
-    return;
+  for(int i=0;i<size;i++){
+    v[i]=abs(v[i]);
+    v[i]=v[i]*v[i];
+  
+     }
+  
+   return;
 }
 int main() {
 vector <int> v;
-int size,left_ptr,right_ptr;
+int size;
 cin>>size;
 for(int i=0;i<size;i++){
     int element;
     cin>>element;
     v.push_back(element);
 }
-swap(v);
+squareandsort(v);
 for(int i=0;i<v.size();i++){
   cout<<v[i]<<" ";
 }
