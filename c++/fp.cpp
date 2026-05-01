5e553284329c3f221e5354edf1268e3ba549c12c
sneha <ponchupd@gmail.com>
2026-05-01 14:12:24 +0530

    sorting zeros and ones complete

--- c++/fp.cpp
+++ c++/fp.cpp
@@ -4,6 +4,23 @@
 #include <cmath>
 #include <string>
 using namespace std;
+void SortZerosAndOnes(vector <int> &v){
+int countzero=0;
+int n=v.size();
+for(int element: v){
+  if(element==0){
+    countzero++;
+  }
+}
+for(int i=0;i<n;i++){
+  if(i<countzero){
+    v[i]=0;
+  }
+  else{
+     v[i]=1;
+   } 
+  }
+}
 int main(){
 int size;
 cin>> size;
@@ -13,9 +30,10 @@ for(int i=0;i<size;i++){
   cin>> element;
   v.push_back(element);
 }
+SortZerosAndOnes(v);
 for(int i=0;i<size;i++){
-  cout<<"vector is "<<v[i]<<" ";
-}
+   cout<<v[i]<<" ";
+   }
 
    cout<<endl;
   return 0;
