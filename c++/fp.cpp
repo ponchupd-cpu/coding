f2ecc629716a928add816be562edefb42881cc9f
sneha <ponchupd@gmail.com>
2026-05-03 21:43:41 +0530

    transpose of matrix

--- c++/fp.cpp
+++ c++/fp.cpp
@@ -15,36 +15,12 @@ int main(){
       cin>> A[i][j];
     }
   }
-  int r2,c2;
-  cout<<"enter r2"<<endl;
-  cout<<"enter c2"<<endl;
-  cin>>r2>>c2;
-  int B[r2][c2];
-  for(int i=0;i<r2;i++){
-    for(int j=0;j<c2;j++){
-      cin>> B[i][j];
-    }
-  }
-  if(c1!=r2){
-    cout<<"matrix multiplication is not possible for this input";
-  }
   
-  int c[r1][c2];
-  for(int i=0;i<r1;i++){   //for multiplication
-    for(int j=0;j<c2;j++){
-      int value=0;
-      for(int k=0;k<c1;k++){
-        value+=A[i][k]*B[k][j];
-      }
-      c[i][j]=value;
-    }
-  }
-  for(int i=0;i<r1;i++){
-    for(int j=0;j<c2;j++){
-      cout<<c[i][j]<<" ";
+  for(int i=0;i<c1;i++){
+    for(int j=0;j<r1;j++){
+      cout<<A[j][i]<<" ";
     }
     cout<<endl;
   }
-  
   return 0;
 }
