4d017e877238c6fdd2cf3e9918658ecd79de8570
sneha <ponchupd@gmail.com>
2026-05-03 12:52:18 +0530

    2D Arrays input

--- c++/fp.cpp
+++ c++/fp.cpp
@@ -5,27 +5,23 @@
 #include <string>
 using namespace std;
 int main(){
-vector <int> v;
-cout<<"enter size of vector?";
-int size;
-cin>> size;
-for(int i=0;i<size;i++){
-  int element;
-  cin>> element;
-  v.push_back(element);
-}
-int queries1,prefixsum1=0;
-cout<<"enter querie1"<<endl;
-cin>> queries1;
-for(int i=0;i<queries1;i++){
-  prefixsum1+=v[i];
-}
-int queries2,prefixsum2=0;
-cout<<"enter querie2"<<endl;
-cin>> queries2;
-for(int i=0;i<queries2;i++){
-  prefixsum2+=v[i];
-}
-cout<<prefixsum1-prefixsum2;
+ int n,m;
+ cout<<"enter number of rows n"<<endl;
+ cout<<"enter number of columns m"<<endl;
+ cin>>n>>m;
+ int array[n][m];
+ for(int i=0;i<n;i++){
+  for(int j=0;j<m;j++){
+    cin>>array[i][j];
+  }
+ }
+ for(int i=0;i<n;i++){
+  for(int j=0;j<m;j++){
+    cout<<array[i][j]<<" ";
+  }
+  cout<<endl;
+ }
+ 
+
   return 0;
 }
