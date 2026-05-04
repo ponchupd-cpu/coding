e52892363105daa8b37ed568567830e6a87eb48b
sneha <ponchupd@gmail.com>
2026-05-04 14:36:40 +0530

    triangle with factorial

--- c++/fp.cpp
+++ c++/fp.cpp
@@ -4,23 +4,28 @@
 #include <cmath>
 #include <string>
 using namespace std;
-int main(){
-  int r1,c1;
-  cout<<"enter r1"<<endl;
-  cout<<"enter c1"<<endl;
-  cin>>r1>>c1;
-  int A[r1][c1];
-  for(int i=0;i<r1;i++){
-    for(int j=0;j<c1;j++){
-      cin>> A[i][j];
-    }
+long long factorial(int n){
+  int fact=1;
+  for(int i=1;i<=n;i++){
+    fact*=i;
   }
-  
-  for(int i=0;i<r1;i++){
-    for(int j=0;j<c1;j++){
-      cout<<A[j][i]<<" ";
+  return fact;
+}
+long long ncr(int n,int r){
+int x;
+x=factorial(n)/(factorial(r)*factorial(n-r));
+  return(x);
+}
+
+int main(){
+  int row;
+  cout<<"enter row number";
+  cin>> row;
+  for(int i=1;i<=row;i++){
+    for(int j=0;j<i;j++){
+      cout<<ncr(i,j)<<" ";
     }
-    cout<<endl;
+     cout<<endl;
   }
   return 0;
 }
