aeb1a35499dd8f4931db669dee039d2d9b49ba2b
sneha <ponchupd@gmail.com>
2026-06-05 14:06:41 +0530

    1873C

--- c++/fp.cpp
+++ c++/fp.cpp
@@ -13,24 +13,32 @@
 using namespace std;
 
 void solve(){
-int n,k;cin>> n>> k;
-vector<int>v(n);
-for(int i=0;i<n;i++){
-    cin>> v[i];
-}
-map<int, int>cnt;
-for(int i=0;i<n;i++){
-    cnt[v[i]]++;
+int count=0;
+for(int i=0;i<10;i++){
+  for(int j=0;j<10;j++){
+    char character;
+    cin>> character;
+    if(character == 'X'){
+      if(i==0||i==9||j==0||j==9){
+        count+=1;
+      }
+      else if(i==1||i==8||j==1||j==8){
+        count+=2;
+      }
+      else if(i==2||i==7||j==2||j==7){
+        count+=3;
+      }
+      else if(i==3||i==6||j==3||j==6){
+        count+=4;
+      }
+      else if(i==4||i==5||j==4||j==5){
+        count+=5;
+      }
+    }
+  }
  }
-int freq=cnt[k];
-if(freq==0){
-    cout<<"No"<<endl;
+ cout<< count <<"\n";
 }
-else{
-    cout<<"Yes"<<endl;
-}
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
