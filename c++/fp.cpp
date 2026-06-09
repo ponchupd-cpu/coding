18067cfc7c8e3a6b95dc90f2b6198b0233a2c045
sneha <ponchupd@gmail.com>
2026-06-09 13:51:45 +0530

    1858A

--- c++/fp.cpp
+++ c++/fp.cpp
@@ -14,24 +14,13 @@
 using namespace std;
 
 void solve(){
- int n;cin>> n;
- vector<int>b(n);
- for(int i=0;i<n;i++){
-  cin>> b[i];
+ int a,b,c;cin>> a>> b>> c;
+ if(a+(c%2)>b){
+    cout<<"First"<<"\n";
  }
- vector<int>a;
- a.push_back(b[0]);
- for(int i=1;i<n;i++){
-  if(b[i]<b[i-1]){
-    a.push_back(b[i]);
-  }
-  a.push_back(b[i]);
+ else{
+   cout<<"Second"<<"\n";
  }
- cout<<a.size()<<"\n";
- for(int i=0;i<a.size();i++){
-  cout<<a[i]<<" ";
- }
- cout <<"\n";
 }
 
 signed main() {
