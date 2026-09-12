ae50e30a12e2be13c500c8381b054935d515e9fe
sneha <ponchupd@gmail.com>
2026-09-13 02:00:28 +0530

    mex multiset.cpp

--- c++/reptide.cpp
+++ c++/reptide.cpp
@@ -10,15 +10,29 @@ using namespace std;
 #define pb push_back
  
 void solve(){
-vector<int>v(3);
-for(int i=0;i<3;i++){
-    cin>>v[i];
+int n;cin>>n;
+vector<int>a(n);
+for(int i=0;i<n;i++){
+    cin>>a[i];
+ }
+ int count=0;
+ for(int i=0;i<n;i++){
+    if(a[i]==0){
+        count++;
+    }
+ }
+if(count<2){
+    cout<<-1<<"\n";
+ }
+else if(a[0]==0 && a[n-1]==0){
+    cout<<0<<"\n";
 }
-sort(v.begin(),v.end());
-int x=v[1]-v[0];
-int y=v[2]-v[1];
-int ans=min(x,y);
-cout<<ans<<"\n";
+else if(a[0]==1&&a[n-1]==1){
+    cout<<2<<"\n";
+}
+else{
+    cout<<0<<"\n";
+}
 }
 int main(){
     ios_base::sync_with_stdio(false);
